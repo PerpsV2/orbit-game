4fcdb9cd83553e21e0d165a408bf9e68a698c77f
PerpsV2 <josephzhao2008@gmail.com>
2026-03-13 02:48:08 -0400

    Add infinite values to BinaryScientificDecimal.cs

--- OrbitGame.Profiling/BinaryScientificDecimal.cs
+++ OrbitGame.Profiling/BinaryScientificDecimal.cs
@@ -2,140 +2,260 @@ using System.Globalization;
 
 namespace OrbitGame.Profiling;
 
+public class PrecisionException : Exception
+{
+    public PrecisionException() { }
+    public PrecisionException(string message) : base(message) { }
+}
+
 /// <summary>
 /// Number with decimal precision but arbitrary place value.
 /// </summary>
-public struct BinaryScientificDecimal : IFormattable
+public struct BinaryScientificDecimal
 {
+    private const int MaxPrecision = 62;
+
     private long _mantissa;
     private int _exponent;
     private int _precision;
+    private readonly bool _infinite;
 
-    public BinaryScientificDecimal(long mantissa, int exponent, int precision)
+    public long Mantissa
     {
-        _mantissa = mantissa;
-        _exponent = exponent;
-        _precision = precision;
-        Normalize();
+        get => _mantissa;
+        private set
+        {
+            if (_infinite) throw new ArithmeticException("Cannot set the mantissa of an infinite ScientificDecimal");
+            _mantissa = value;
+        }
+    }
+    public int Exponent
+    {
+        get => _exponent;
+        private set
+        {
+            if (_infinite) throw new ArithmeticException("Cannot set the mantissa of an infinite ScientificDecimal");
+            _exponent = value;
+        }
+    }
+    public int Precision
+    {
+        get => _precision;
+        private set
+        {
+            if (_infinite) throw new ArithmeticException("Cannot set the mantissa of an infinite ScientificDecimal");
+            _precision = value;
+        }
     }
 
-    public static BinaryScientificDecimal FromDecimal(decimal mantissa, int exponent, int precision)
+    public static BinaryScientificDecimal Zero { get; } = new BinaryScientificDecimal(0, 0, MaxPrecision);
+    public static BinaryScientificDecimal One { get; } = new BinaryScientificDecimal(1, 0, MaxPrecision);
+    public static BinaryScientificDecimal AdditiveIdentity { get; } = Zero;
+    public static BinaryScientificDecimal MultiplicativeIdentity { get; } = One;
+    public static BinaryScientificDecimal PositiveInfinity { get; } = new(true, true);
+    public static BinaryScientificDecimal NegativeInfinity { get; } = new(true, false);
+    
+    public static int Radix { get; } = 2;
+    
+
+    public BinaryScientificDecimal(long mantissa, int exponent, int precision = MaxPrecision)
     {
-        throw new NotImplementedException();
+        Mantissa = mantissa;
+        Exponent = exponent;
+        Precision = precision;
+        Normalize();
+    }
+    
+    public BinaryScientificDecimal(decimal mantissa, int exponent)
+        => this = FromDecimal(mantissa, exponent);
+
+    public BinaryScientificDecimal(int exponent)
+        => this = FromDecimal(1, exponent);
+
+    private BinaryScientificDecimal(bool isInfinite, bool isPositive)
+    {
+        Mantissa = isPositive ? 1 : -1;
+        _infinite = isInfinite;
+    }
+    
+    public readonly bool Positive => long.IsPositive(_mantissa);
+    public readonly bool Negative => long.IsNegative(_mantissa);
+
+    private static BinaryScientificDecimal FromDecimal(decimal mantissa, int exponent)
+    {
+        mantissa *= (decimal)Math.Pow(10, exponent);
+        int decimalPlaces = BitConverter.GetBytes(decimal.GetBits(mantissa)[3])[2];
+        mantissa *= (decimal)Math.Pow(10, decimalPlaces);
+        BinaryScientificDecimal result = new BinaryScientificDecimal((long)mantissa, 0);
+        for (int i = 0; i < decimalPlaces; ++i)
+            result /= new BinaryScientificDecimal(10, 0);
+        return result;
     }
 
     private void Normalize()
     {
-        while (Math.Abs(_mantissa) < 1L << (_precision - 1))
+        while (Math.Abs(Mantissa) < 1L << (Precision - 1))
         {
-            _mantissa <<= 1;
-            _exponent -= 1;
+            Mantissa <<= 1;
+            Exponent -= 1;
         }
 
-        while (Math.Abs(_mantissa) > 1L << _precision)
+        while (Math.Abs(Mantissa) > 1L << Precision)
         {
-            _mantissa >>= 1;
-            _exponent += 1;
+            Mantissa >>= 1;
+            Exponent += 1;
         }
     }
 
     private void IncreaseExponent(int goal)
     {
-        bool negative = _mantissa < 0;
-        if (negative) _mantissa = -_mantissa;
-        if (_exponent == goal) return;
-        if (_exponent > goal) throw new ArithmeticException();
-        _mantissa >>= goal - _exponent;
-        _precision -= goal - _exponent;
-        _exponent = goal;
-        if (negative) _mantissa = -_mantissa;
+        bool negative = Mantissa < 0;
+        if (negative) Mantissa = -Mantissa;
+        if (Exponent == goal) return;
+        if (Exponent > goal) throw new ArithmeticException();
+        Mantissa >>= goal - Exponent;
+        Precision -= goal - Exponent;
+        Exponent = goal;
+        if (negative) Mantissa = -Mantissa;
     }
 
     private static BinaryScientificDecimal Add(BinaryScientificDecimal left, BinaryScientificDecimal right)
     {
-        if (left._exponent < right._exponent) left.IncreaseExponent(right._exponent);
-        else if (right._exponent < left._exponent) right.IncreaseExponent(left._exponent);
-        return new(left._mantissa + right._mantissa, 
-            int.Max(left._exponent, right._exponent), 
-            int.Min(left._precision, right._precision));
-    }
-
-    private static BinaryScientificDecimal MultiplyOld(BinaryScientificDecimal left, BinaryScientificDecimal right)
-    {
-        bool leftNegative = left._mantissa < 0;
-        bool rightNegative = right._mantissa < 0;
-        if (leftNegative) left._mantissa *= -1;
-        if (rightNegative) right._mantissa *= -1;
-        int resultPrecision = int.Min(left._precision, right._precision);
-        long resultMantissa = 0b0L;
-        for (int i = 0; i < resultPrecision; ++i)
+        if (left._infinite && right._infinite)
         {
-            for (int j = 0; j <= i; ++j)
-            {
-                long leftDigitValue = (left._mantissa >> (left._precision - 1 - j)) & 1;
-                long rightDigitValue = (right._mantissa >> (right._precision - 1 - (i - j))) & 1;
-                if ((leftDigitValue & rightDigitValue) == 1) resultMantissa += 0b1L << (resultPrecision - i);
-            }
+            if (left.Positive == right.Positive) return left;
+            throw new ArithmeticException("Cannot add opposite signed scientific decimals");
         }
-        int resultExponent = left._precision + left._exponent + right._precision + right._exponent - resultPrecision - 2;
-        if (leftNegative ^ rightNegative) resultMantissa *= -1;
-        return new(resultMantissa, resultExponent, resultPrecision);
+        if (left._infinite) return left;
+        if (right._infinite) return right;
+        
+        if (left.Exponent < right.Exponent) left.IncreaseExponent(right.Exponent);
+        else if (right.Exponent < left.Exponent) right.IncreaseExponent(left.Exponent);
+        return new(left.Mantissa + right.Mantissa, 
+            int.Max(left.Exponent, right.Exponent), 
+            int.Min(left.Precision, right.Precision));
     }
 
     private static BinaryScientificDecimal Multiply(BinaryScientificDecimal left, BinaryScientificDecimal right)
     {
-        bool leftNegative = left._mantissa < 0;
-        bool rightNegative = right._mantissa < 0;
-        if (leftNegative) left._mantissa *= -1;
-        if (rightNegative) right._mantissa *= -1;
-        int resultPrecision = int.Min(left._precision, right._precision);
-        left._mantissa >>= left._precision - resultPrecision / 2;
-        right._mantissa >>= right._precision - resultPrecision / 2 - resultPrecision % 2;
-        long resultMantissa = left._mantissa * right._mantissa;
-        int resultExponent = left._exponent + left._precision + right._exponent + right._precision - resultPrecision;
+        if (left._mantissa == 0 || right._mantissa == 0) return Zero;
+        if (left._infinite || right._infinite) return new(true, 
+            (left.Positive && right.Positive) || (left.Negative && right.Negative));
+        
+        bool leftNegative = left.Mantissa < 0;
+        bool rightNegative = right.Mantissa < 0;
+        if (leftNegative) left.Mantissa *= -1;
+        if (rightNegative) right.Mantissa *= -1;
+        int resultPrecision = int.Min(left.Precision, right.Precision);
+        left.Mantissa >>= left.Precision - resultPrecision / 2;
+        right.Mantissa >>= right.Precision - resultPrecision / 2 - resultPrecision % 2;
+        long resultMantissa = left.Mantissa * right.Mantissa;
+        int resultExponent = left.Exponent + left.Precision + right.Exponent + right.Precision - resultPrecision;
         if (leftNegative ^ rightNegative) resultMantissa *= -1;
         return new BinaryScientificDecimal(resultMantissa, resultExponent, resultPrecision);
     }
 
-    private static BinaryScientificDecimal Divide(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    private static BinaryScientificDecimal Divide(BinaryScientificDecimal dividend, BinaryScientificDecimal divisor)
     {
-        bool leftNegative = left._mantissa < 0;
-        bool rightNegative = right._mantissa < 0;
-        if (leftNegative) left._mantissa *= -1;
-        if (rightNegative) right._mantissa *= -1;
-        int resultPrecision = int.Min(left._precision, right._precision);
-        if (left._exponent < right._exponent) left.IncreaseExponent(right._exponent);
-        else if (right._exponent < left._exponent) right.IncreaseExponent(left._exponent);
+        if (divisor._mantissa == 0 && dividend._mantissa == 0)
+            throw new ArithmeticException("Cannot divide zero by zero");
+        if (divisor._mantissa == 0) return PositiveInfinity * (dividend.Positive ? new(1, 0) : new(-1, 0));
+        if (divisor._infinite && dividend._infinite)
+            throw new ArithmeticException("Cannot divide an infinite value by an infinite value");
+        if (dividend._infinite) return dividend;
+        if (divisor._infinite) return Zero;
+        
+        bool leftNegative = dividend.Mantissa < 0;
+        bool rightNegative = divisor.Mantissa < 0;
+        if (leftNegative) dividend.Mantissa *= -1;
+        if (rightNegative) divisor.Mantissa *= -1;
+        int resultPrecision = int.Min(dividend.Precision, divisor.Precision);
+        if (dividend.Exponent < divisor.Exponent) dividend.IncreaseExponent(divisor.Exponent);
+        else if (divisor.Exponent < dividend.Exponent) divisor.IncreaseExponent(dividend.Exponent);
         long resultMantissa = 0b0L;
-        right._mantissa <<= left._precision - right._precision;
+        divisor.Mantissa <<= dividend.Precision - divisor.Precision;
         for (int i = 0; i < resultPrecision; ++i)
         {
-            if (left._mantissa == 0) break;
-            if (right._mantissa <= left._mantissa)
+            if (dividend.Mantissa == 0) break;
+            if (divisor.Mantissa <= dividend.Mantissa)
             {
-                left._mantissa -= right._mantissa;
+                dividend.Mantissa -= divisor.Mantissa;
                 resultMantissa += 0b1L << resultPrecision - i - 1;
             }
-            right._mantissa >>= 1;
+            divisor.Mantissa >>= 1;
         }
-        int resultExponent = left._precision + left._exponent - right._precision - right._exponent - resultPrecision + 1;
+        int resultExponent = dividend.Precision + dividend.Exponent - divisor.Precision - divisor.Exponent - resultPrecision + 1;
         if (leftNegative ^ rightNegative) resultMantissa *= -1;
         return new(resultMantissa, resultExponent, resultPrecision);
+    }
+
+    private static BinaryScientificDecimal Modulo(BinaryScientificDecimal value, BinaryScientificDecimal mod)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static BinaryScientificDecimal Square(BinaryScientificDecimal value)
+        => value * value;
+
+    private static BinaryScientificDecimal IntPow(BinaryScientificDecimal value, uint power)
+    {
+        BinaryScientificDecimal result = One;
+        for (uint i = 0; i < power; ++i)
+            result *= value;
+        return result;
+    }
+
+    private static BinaryScientificDecimal Sqrt(BinaryScientificDecimal value)
+    {
+        throw new NotImplementedException();
     }
 
     public static BinaryScientificDecimal operator +(BinaryScientificDecimal value)
         => value;
     public static BinaryScientificDecimal operator -(BinaryScientificDecimal value)
-        => new(-value._mantissa, value._exponent, value._precision);
+        => new(-value.Mantissa, value.Exponent, value.Precision);
     public static BinaryScientificDecimal operator +(BinaryScientificDecimal left, BinaryScientificDecimal right)
         => Add(left, right);
     public static BinaryScientificDecimal operator -(BinaryScientificDecimal left, BinaryScientificDecimal right)
         => Add(left, -right);
+    public static BinaryScientificDecimal operator ++(BinaryScientificDecimal value)
+        => Add(value, One);
+    public static BinaryScientificDecimal operator --(BinaryScientificDecimal value)
+        => Add(value, -One);
     public static BinaryScientificDecimal operator *(BinaryScientificDecimal left, BinaryScientificDecimal right)
         => Multiply(left, right);
-
     public static BinaryScientificDecimal operator /(BinaryScientificDecimal left, BinaryScientificDecimal right)
         => Divide(left, right);
+    
+    public static BinaryScientificDecimal MaxMagnitude(BinaryScientificDecimal x, BinaryScientificDecimal y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BinaryScientificDecimal MaxMagnitudeNumber(BinaryScientificDecimal x, BinaryScientificDecimal y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BinaryScientificDecimal MinMagnitude(BinaryScientificDecimal x, BinaryScientificDecimal y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BinaryScientificDecimal MinMagnitudeNumber(BinaryScientificDecimal x, BinaryScientificDecimal y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Equals(BinaryScientificDecimal other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static BinaryScientificDecimal operator %(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    {
+        throw new NotImplementedException();
+    }
 
     public override string ToString()
     {
@@ -152,13 +272,44 @@ public struct BinaryScientificDecimal : IFormattable
         switch (format.ToUpperInvariant())
         {
             case "B":
-                return $"M:{_mantissa:B}, E:{_exponent}, P:{_precision}";
+                return $"M:{Mantissa:B}, E:{Exponent}, P:{Precision}";
             case "G":
-                return $"M:{_mantissa}, E:{_exponent}, P:{_precision}";
+                return $"M:{Mantissa}, E:{Exponent}, P:{Precision}";
             case "N":
-                return $"N:{_mantissa * Math.Pow(2, _exponent)}";
+                return $"N:{Mantissa * Math.Pow(2, Exponent)}";
             default:
                 throw new FormatException();
         }
+    }
+
+    
+    public static bool operator ==(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool operator !=(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool operator >(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool operator >=(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool operator <(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool operator <=(BinaryScientificDecimal left, BinaryScientificDecimal right)
+    {
+        throw new NotImplementedException();
     }
 }
--- OrbitGame.Profiling/Program.cs
+++ OrbitGame.Profiling/Program.cs
@@ -4,9 +4,9 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using OrbitGame.Profiling;
 
-var value1 = new BinaryScientificDecimal(0b1L, 0, 62);
-var value2 = new BinaryScientificDecimal(0b1L, 5, 62);
-var result = value1 * value2;
+var value1 = new BinaryScientificDecimal(1, 5);
+var value2 = new BinaryScientificDecimal(1, -9);
+var result = value1 / value2;
 var format = "N";
 Console.WriteLine(value1.ToString(format));
 Console.WriteLine(value2.ToString(format));
@@ -18,17 +18,17 @@ public class Profiler
 {
     private static readonly Random Rnd = new();
     
-    private readonly BinaryScientificDecimal _left = new(Rnd.NextInt64(long.MinValue, long.MaxValue), Rnd.Next(-3000, 3000), 62);
-    private readonly BinaryScientificDecimal _right = new(Rnd.NextInt64(long.MinValue, long.MaxValue), Rnd.Next(-3000, 3000), 62);
+    private readonly BinaryScientificDecimal _left = new(Rnd.NextInt64(long.MinValue, long.MaxValue), Rnd.Next(-3000, 3000));
+    private readonly BinaryScientificDecimal _right = new(Rnd.NextInt64(long.MinValue, long.MaxValue), Rnd.Next(-3000, 3000));
 
     private readonly DecimallyScientificDecimal _sdLeft = new((decimal)Rnd.NextDouble(), Rnd.Next(-3000, 3000));
     private readonly DecimallyScientificDecimal _sdRight = new((decimal)Rnd.NextDouble(), Rnd.Next(-3000, 3000));
     
-    //[Benchmark]
+    [Benchmark]
     public BinaryScientificDecimal BSD_Add()
         => _left + _right;
     
-    //[Benchmark]
+    [Benchmark]
     public BinaryScientificDecimal BSD_Subtract()
         => _left - _right;
     
@@ -36,15 +36,15 @@ public class Profiler
     public BinaryScientificDecimal BSD_Multiply()
         => _left * _right;
     
-    //[Benchmark]
+    [Benchmark]
     public BinaryScientificDecimal BSD_Divide()
         => _left / _right;
     
-    //[Benchmark]
+    [Benchmark]
     public DecimallyScientificDecimal SD_Add()
         => _sdLeft + _sdRight;
     
-    //[Benchmark]
+    [Benchmark]
     public DecimallyScientificDecimal SD_Subtract()
         => _sdLeft - _sdRight;
     
@@ -52,7 +52,7 @@ public class Profiler
     public DecimallyScientificDecimal SD_Multiply()
         => _sdLeft * _sdRight;
 
-    //[Benchmark]
+    [Benchmark]
     public DecimallyScientificDecimal SD_Divide()
         => _sdLeft / _sdRight;
 }
