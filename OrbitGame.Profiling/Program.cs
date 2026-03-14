9ac3349ba8c6fc30e7ec18d14327233b31698249
PerpsV2 <josephzhao2008@gmail.com>
2026-03-14 17:45:55 -0400

    Add methods to convert between base 10 and BinaryScientificDecimal.cs (now ScientificDecimal.cs)

--- OrbitGame.Profiling/Program.cs
+++ OrbitGame.Profiling/Program.cs
@@ -4,32 +4,4 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using OrbitGame.Profiling;
 
-var value1 = new BinaryScientificDecimal(0b1101100101110101, -1);
-var value2 = new BinaryScientificDecimal(0b111, -2);
-//var result = value1 % value2;
-var format = "N";
-Console.WriteLine(value1.ToString(format));
-//Console.WriteLine(value2.ToString(format));
-//Console.WriteLine(result.ToString(format));
-Console.WriteLine(BinaryScientificDecimal.Sqrt(value1).ToString(format));
-
-//var summary = BenchmarkRunner.Run<Profiler>();
-
-public class Profiler
-{
-    private static readonly Random Rnd = new();
-    
-    private readonly BinaryScientificDecimal _left = new(Rnd.NextInt64(long.MinValue, long.MaxValue), Rnd.Next(-3000, 3000));
-    private readonly BinaryScientificDecimal _right = new(Rnd.NextInt64(long.MinValue, long.MaxValue), Rnd.Next(-3000, 3000));
-
-    private readonly ScientificDecimal _sdLeft = new(Rnd.NextDouble(), Rnd.Next(-3000, 3000));
-    private readonly ScientificDecimal _sdRight = new(Rnd.NextDouble(), Rnd.Next(-3000, 3000));
-    
-    [Benchmark]
-    public BinaryScientificDecimal BSD_Mod()
-        => _left % _right;
-    
-    [Benchmark]
-    public ScientificDecimal SD_Mod()
-        => _sdLeft % _sdRight;
-}
+var value1 = new ScientificDecimal(3.14159265358979323, 310410000);
