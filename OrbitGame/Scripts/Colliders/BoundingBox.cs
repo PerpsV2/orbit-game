3af86df9037b852fffee883cb6005aa9ff5ec676
PerpsV2 <josephzhao2008@gmail.com>
2026-03-31 00:31:54 -0400

    Add documentation to BoundingBox.cs

--- OrbitGame/Scripts/Colliders/BoundingBox.cs
+++ OrbitGame/Scripts/Colliders/BoundingBox.cs
@@ -3,20 +3,34 @@ namespace OrbitGame;
 /// <summary>
 /// Represents an axis-aligned bounding box for a game object.
 /// </summary>
-/// <param name="Left">Positive distance from the center to the left edge of the BoundingBox</param>
-/// <param name="Right">Positive distance from the center to the right edge of the BoundingBox</param>
-/// <param name="Top">Positive distance from the center to the top edge of the BoundingBox</param>
-/// <param name="Bottom">Positive distance from the center to the bottom edge of the BoundingBox</param>
+/// <param name="Left">Positive distance from the center to the left edge of the BoundingBox.</param>
+/// <param name="Right">Positive distance from the center to the right edge of the BoundingBox.</param>
+/// <param name="Top">Positive distance from the center to the top edge of the BoundingBox.</param>
+/// <param name="Bottom">Positive distance from the center to the bottom edge of the BoundingBox.</param>
 public readonly record struct BoundingBox(
     SDecimal Left,
     SDecimal Right,
     SDecimal Top,
     SDecimal Bottom)
 {
+    
+    /// <summary>
+    /// Create a bounding box from a center location, a width, and a height.
+    /// </summary>
+    /// <param name="center">Center of the bounding box.</param>
+    /// <param name="width">Width of the bounding box.</param>
+    /// <param name="height">Height of the bounding box.</param>
     public BoundingBox(Vec2<SDecimal> center, SDecimal width, SDecimal height) : 
         this(center.X - width / 2, center.X + width / 2, center.Y + height / 2, center.Y - height / 2)
     { }
 
+    /// <summary>
+    /// Returns whether two bounding boxes are intersecting with each other.
+    /// </summary>
+    /// <param name="other">The other bounding box.</param>
+    /// <param name="referenceSpatial">The SpatialInfo of the first bounding box.</param>
+    /// <param name="incidentSpatial">The SpatialInfo of the second bounding box.</param>
+    /// <returns>Whether the two bounding boxes intersect.</returns>
     public bool IntersectsWith(BoundingBox other, SpatialInfo referenceSpatial, SpatialInfo incidentSpatial)
     {
         if (IsEmpty() || other.IsEmpty()) return false;
@@ -26,6 +40,10 @@ public readonly record struct BoundingBox(
                referenceSpatial.Position.Y + Top >= incidentSpatial.Position.Y + other.Bottom;
     }
 
+    /// <summary>
+    /// Returns whether a bounding box is empty or not.
+    /// </summary>
+    /// <returns>Whether the bounding box is empty or not.</returns>
     public bool IsEmpty()
     {
         return Left == Right || Top == Bottom;
