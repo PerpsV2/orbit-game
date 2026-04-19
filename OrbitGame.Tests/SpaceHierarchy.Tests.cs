89d0a03db2d86aeb9403a5ab65d72b448a678a79
PerpsV2 <josephzhao2008@gmail.com>
2026-04-18 22:12:20 -0400

    Add kD tree class to represent game object hierarchy

---
+++ OrbitGame.Tests/SpaceHierarchy.Tests.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OrbitGame.Tests;
+
+public class SpaceHierarchy_Tests
+{
+    private class TestKinematicObject(string identifier, SpatialInfo spatialInfo)
+        : KinematicObject(identifier, spatialInfo)
+    {
+        public class TestTemplate : KinematicObjectTemplate
+        {
+            public new static Dictionary<string, TestKinematicObject> AllInstances { get; } = new();
+            
+            protected override void AddInstance(string identifier, KinematicObject instance)
+            {
+                base.AddInstance(identifier, instance);
+                if (!AllInstances.TryAdd(identifier, (TestKinematicObject)instance))
+                    throw new ArgumentException($"KinematicObject with identifier '{identifier}' has already been added");
+            }
+
+            public override void DestroyInstance(string identifier)
+            {
+                base.DestroyInstance(identifier);
+                AllInstances.Remove(identifier);
+                Instances.Remove(identifier);
+            }
+            
+            public TestKinematicObject CreateTestInstance(string identifier, SpatialInfo spatialInfo)
+            {
+                TestKinematicObject instance = new TestKinematicObject(identifier, spatialInfo);
+                AddInstance(identifier, instance);
+                return instance;
+            }
+        }
+    }
+
+    private class TestDerivedKinematicObject(string identifier, SpatialInfo spatialInfo)
+        : TestKinematicObject(identifier, spatialInfo)
+    {
+        public class TestDerivedTemplate : KinematicObjectTemplate
+        {
+            public new static Dictionary<string, TestDerivedKinematicObject> AllInstances { get; } = new();
+            
+            protected override void AddInstance(string identifier, KinematicObject instance)
+            {
+                base.AddInstance(identifier, instance);
+                if (!AllInstances.TryAdd(identifier, (TestDerivedKinematicObject)instance))
+                    throw new ArgumentException($"KinematicObject with identifier '{identifier}' has already been added");
+            }
+
+            public override void DestroyInstance(string identifier)
+            {
+                base.DestroyInstance(identifier);
+                AllInstances.Remove(identifier);
+                Instances.Remove(identifier);
+            }
+            
+            public TestDerivedKinematicObject CreateTestInstance(string identifier, SpatialInfo spatialInfo)
+            {
+                TestDerivedKinematicObject instance = new TestDerivedKinematicObject(identifier, spatialInfo);
+                AddInstance(identifier, instance);
+                return instance;
+            }
+        }
+    }
+    
+    private readonly ITestOutputHelper _output;
+    private readonly TestKinematicObject.TestTemplate _template = new();
+    private readonly TestDerivedKinematicObject.TestDerivedTemplate _derivedTemplate = new();
+    private readonly SpaceHierarchy _randomHierarchy;
+    private readonly SpaceHierarchy _testHierarchy;
+    private readonly SpaceHierarchy.Node _xNode;
+    private readonly SpaceHierarchy.Node _yNode;
+    private readonly Random _rnd = new();
+    
+    public SpaceHierarchy_Tests(ITestOutputHelper output)
+    {
+        KinematicObject.KinematicObjectTemplate.DestroyAll();
+        
+        _output = output;
+        List<KinematicObject> points = new();
+        for (int i = 0; i < 100; ++i)
+        {
+            points.Add(_template.CreateTestInstance("Test Instance " + i, new SpatialInfo(
+                position: new Vec2<SDecimal>(_rnd.NextDouble() * 100, _rnd.NextDouble() * 100)
+            )));
+        }
+        _randomHierarchy = new SpaceHierarchy(points);
+        
+        /* ----- Test kD tree layout -----
+         * 
+         *   0 1 2 3 4 5 6 7 8 9
+         * 0 . . . . | . . . . .
+         * 1 . . . . | . . . . .
+         * 2 . # . . | . . . . .
+         * 3 . . . . | . # . . .
+         * 4 . . . . | . . . . .
+         * 5 ________| . . . . .
+         * 6 . # | . |__________
+         * 7 . . | # | . . . . .
+         * 8 . . | . | . . . . .
+         * 9 . . | . | . . . . #
+         */
+        _testHierarchy = new SpaceHierarchy([
+            _template.CreateTestInstance("Test Instance A", new SpatialInfo(position: new Vec2<SDecimal>(1, 2))),
+            _template.CreateTestInstance("Test Instance B", new SpatialInfo(position: new Vec2<SDecimal>(6, 3))),
+            _template.CreateTestInstance("Test Instance C", new SpatialInfo(position: new Vec2<SDecimal>(1, 6))),
+            _template.CreateTestInstance("Test Instance D", new SpatialInfo(position: new Vec2<SDecimal>(3, 7))),
+            _template.CreateTestInstance("Test Instance E", new SpatialInfo(position: new Vec2<SDecimal>(9, 9))),
+        ]);
+        _xNode = new SpaceHierarchy.Node(0, 5, null, null, new());
+        _yNode = new SpaceHierarchy.Node(1, 5, null, null, new());
+    }
+
+    [Fact]
+    public void SpaceHierarchy_Constructor()
+    {
+        _output.WriteLine(_randomHierarchy.ToString());
+        _output.WriteLine(_testHierarchy.ToString());
+    }
+
+    [Fact]
+    public void SpaceHierarchy_AddObject()
+    {
+        _output.WriteLine(_testHierarchy.ToString());
+        _testHierarchy.AddObject(
+            _derivedTemplate.CreateTestInstance("Test Instance F", new SpatialInfo(position: new Vec2<SDecimal>(3, 9)))
+        );
+        _output.WriteLine(_testHierarchy.ToString());
+    }
+    
+    [Fact]
+    public void SpaceHierarchy_RemoveObject()
+    {
+        _output.WriteLine(_testHierarchy.ToString());
+        _testHierarchy.RemoveObject("Test Instance E");
+        _output.WriteLine(_testHierarchy.ToString());
+    }
+
+    [Fact]
+    public void SpaceHierarchyNode_GetSideMethod()
+    {
+        Vec2<SDecimal> leftXNodePosition = new Vec2<SDecimal>(0, 5);
+        Vec2<SDecimal> medianXNodePosition = new Vec2<SDecimal>(5, 0);
+        Vec2<SDecimal> rightXNodePosition = new Vec2<SDecimal>(10, 0);
+        
+        Vec2<SDecimal> leftYNodePosition = new Vec2<SDecimal>(5, 0);
+        Vec2<SDecimal> medianYNodePosition = new Vec2<SDecimal>(0, 5);
+        Vec2<SDecimal> rightYNodePosition = new Vec2<SDecimal>(0, 10);
+        
+        Assert.Equal(-1, _xNode.GetSide(leftXNodePosition));
+        Assert.Equal(0, _xNode.GetSide(medianXNodePosition));
+        Assert.Equal(1, _xNode.GetSide(rightXNodePosition));
+        
+        Assert.Equal(-1, _yNode.GetSide(leftYNodePosition));
+        Assert.Equal(0, _yNode.GetSide(medianYNodePosition));
+        Assert.Equal(1, _yNode.GetSide(rightYNodePosition));
+    }
+
+    [Fact]
+    public void SpaceHierarchyNode_SplitMethod()
+    {
+        
+    }
+
+    [Fact]
+    public void SpaceHierarchyNode_IsLeafMethod()
+    {
+        Assert.False(_testHierarchy.RootNode?.Right?.IsLeaf());
+        Assert.True(_testHierarchy.RootNode?.Right?.Right?.IsLeaf());
+    }
+}
--- OrbitGame/Scripts/SpaceHierarchy.cs
+++ OrbitGame/Scripts/SpaceHierarchy.cs
@@ -1,9 +1,209 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace OrbitGame;
 
 public class SpaceHierarchy
 {
-    public class Node
+    public class Node(int axis, SDecimal? median, Node? left, Node? right, List<KinematicObject> objList)
     {
+        public readonly int Axis = axis;
+        public SDecimal? Median = median;
+        public Node? Left = left;
+        public Node? Right = right;
+        public readonly List<KinematicObject> ObjectList = objList;
+
+        public Node(int? axis, KinematicObject obj)
+            : this(axis ?? 0, null, null, null, [obj])
+        { }
         
+        /// <summary>
+        /// Returns the side of a node a coordinate is on.
+        /// </summary>
+        /// <param name="position">Coordinate to find the side of.</param>
+        /// <returns>
+        /// <para>-1 - Object is on the left side of the median (lesser side).</para>
+        /// <para> 0 - Object is on the median.</para>
+        /// <para>+1 - Object is on the right side of the median (greater side).</para>
+        /// </returns>
+        /// <exception cref="NullReferenceException">Node is a leaf node and has no median.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Node has an invalid axis.</exception>
+        public int GetSide(Vec2<SDecimal> position)
+        {
+            if (Median is null) throw new NullReferenceException("Cannot access side of kD-tree node with no median");
+            SDecimal comparisonVariable = Axis == 0 ? position.X : Axis == 1 ? position.Y : 
+                throw new ArgumentOutOfRangeException(nameof(Axis), "Node axis is out of range");
+            return comparisonVariable < Median ? -1 : comparisonVariable > Median.Value ? 1 : 0;
+        }
+
+        public void AddKinematicObject(KinematicObject newObj)
+        {
+            KinematicObject? leafObject = GetLeafObject();
+            if (leafObject is null) throw new Exception("Cannot add object to branch node");
+            if (Axis == 0) Median = (leafObject.Position.X + newObj.Position.X) / 2;
+            if (Axis == 1) Median = (leafObject.Position.Y + newObj.Position.Y) / 2;
+
+            int childAxis = Axis == 0 ? 1 : 0;
+            
+            if (GetSide(newObj.Position) <= 0)
+            {
+                Left = new Node(childAxis, newObj);
+                Right = new Node(childAxis, leafObject);
+            }
+
+            if (GetSide(newObj.Position) > 0)
+            {
+                Left = new Node(childAxis, leafObject);
+                Right = new Node(childAxis, newObj);
+            }
+        }
+
+        public bool IsLeaf() 
+            => Median is null;
+        
+        private KinematicObject? GetLeafObject()
+        {
+            if (!IsLeaf()) return null;
+            return ObjectList[0];
+        }
+    }
+
+    private Dictionary<string, KinematicObject> _objects;
+    private Dictionary<Type, Dictionary<string, KinematicObject>> _objectTypes;
+    public Node? RootNode;
+    
+    public SpaceHierarchy(List<KinematicObject> objList)
+    {
+        RootNode = ConstructKdTree(objList);
+        _objects = new Dictionary<string, KinematicObject>();
+        _objectTypes = new Dictionary<Type, Dictionary<string, KinematicObject>>();
+        foreach (var obj in objList) AddObjectToObjectList(obj);
+    }
+
+    public Dictionary<string, KinematicObject> GetAllObjects()
+        => _objects;
+
+    public Dictionary<string, KinematicObject> GetAllObjectsOfType<T>() where T : KinematicObject
+        => _objectTypes[typeof(T)];
+    
+    public Node TraverseTreeToPosition(Vec2<SDecimal> position, Action<Node>? action = null)
+    {
+        if (RootNode is null) throw new NullReferenceException("Root node does not exist");
+        
+        Node currentNode = RootNode;
+        while (true)
+        {
+            if (currentNode.IsLeaf()) return currentNode;
+            action?.Invoke(currentNode);
+            int side = currentNode.GetSide(position);
+            if (side <= 0) currentNode = currentNode.Left ?? throw new NullReferenceException("Non-leaf node has no children");
+            if (side > 0) currentNode = currentNode.Right ?? throw new NullReferenceException("Non-leaf node has no children");
+        }
+    }
+
+    public Node TraverseTreeToIdentifier(string identifier, Action<Node>? action = null)
+    {
+        return TraverseTreeToPosition(_objects[identifier].Position, action);
+    }
+
+    public void RemoveObject(string identifier)
+    {
+        RemoveObjectFromKdTree(identifier);
+        RemoveObjectFromObjectList(identifier);
+    }
+
+    private void RemoveObjectFromObjectList(string identifier)
+    {
+        _objects.Remove(identifier);
+        foreach (var type in _objectTypes)
+            type.Value.Remove(identifier);
+    }
+
+    private void RemoveObjectFromKdTree(string identifier)
+    {
+        if (RootNode is null) throw new NullReferenceException("Root node does not exist");
+        Node parentNode = RootNode;
+        TraverseTreeToIdentifier(identifier, node => {
+                node.ObjectList.RemoveAll(obj => obj.Identifier == identifier);
+                parentNode = node;
+            });
+        parentNode.Left = null;
+        parentNode.Right = null;
+        parentNode.Median = null;
+    }
+
+    public void AddObject(KinematicObject obj)
+    {
+        AddObjectToObjectList(obj);
+        AddObjectToKdTree(obj);
+    }
+
+    private void AddObjectToObjectList(KinematicObject obj)
+    {
+        _objects.Add(obj.Identifier, obj);
+        Type? currentType = obj.GetType();
+        do
+        {
+            if (currentType is null) break;
+            _objectTypes.TryAdd(currentType, new Dictionary<string, KinematicObject>());
+            _objectTypes[currentType].Add(obj.Identifier, obj);
+            currentType = currentType.BaseType;
+        } while (currentType != typeof(KinematicObject));
+    }
+
+    private void AddObjectToKdTree(KinematicObject obj)
+    {
+        // if the space hierarchy does not exist, create one.
+        if (RootNode == null)
+        {
+            RootNode = new Node(null, obj);
+            return;
+        }
+
+        Node leafNode = TraverseTreeToPosition(obj.Position, node => node.ObjectList.Add(obj));
+        leafNode.AddKinematicObject(obj);
+    }
+    
+    public void ReconstructKdTree()
+    {
+        RootNode = ConstructKdTree(_objects.Values.ToList());
+    }
+
+    private Node ConstructKdTree(List<KinematicObject> objList, int depth = 0)
+    {
+        int axis = depth % 2;
+
+        if (objList.Count == 1) return new Node(axis, objList[0]);
+
+        switch (axis)
+        {
+            case 0:
+            {
+                objList = objList.OrderBy(obj => obj.Position.X).ToList();
+                SDecimal median = objList.Aggregate((SDecimal)0, (total, next) => total + next.Position.X) / objList.Count;
+
+                Node node = new Node(axis, median,
+                    ConstructKdTree(objList.Where(obj => obj.Position.X <= median).ToList(), depth + 1),
+                    ConstructKdTree(objList.Where(obj => obj.Position.X > median).ToList(), depth + 1), 
+                    objList
+                );
+                return node;
+            }
+            case 1:
+            {
+                objList = objList.OrderBy(obj => obj.Position.Y).ToList();
+                SDecimal median = objList.Aggregate((SDecimal)0, (total, next) => total + next.Position.Y) / objList.Count;
+
+                Node node = new Node(axis, median,
+                    ConstructKdTree(objList.Where(obj => obj.Position.Y <= median).ToList(), depth + 1),
+                    ConstructKdTree(objList.Where(obj => obj.Position.Y > median).ToList(), depth + 1), 
+                    objList
+                );
+                return node;
+            }
+            default:
+                throw new ArgumentOutOfRangeException(nameof(axis), axis, "KdTree axis is invalid");
+        }
     }
 }
