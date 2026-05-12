a99af1c7160fd26dac92c8ba6e45dbcb311cd5a0
PerpsV2 <109924625+PerpsV2@users.noreply.github.com>
2026-05-12 08:43:56 -0700

    Add unit tests for qQEngine

---
+++ qQEngine.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace qQEngine.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void SDecimal_Constructor()
+    {
+        SDecimal sDecimal = new SDecimal();
+        Assert.Equal(0, sDecimal.Mantissa);
+        Assert.Equal(0, sDecimal.Exponent);
+        Assert.Equal(SDecimal.Zero, sDecimal);
+
+        sDecimal = new SDecimal(0, 0);
+        Assert.Equal(0, sDecimal.Mantissa);
+        Assert.Equal(0, sDecimal.Exponent);
+        Assert.Equal(SDecimal.Zero, sDecimal);
+
+        sDecimal = new SDecimal(1, 0);
+        Assert.Equal(0, sDecimal.Mantissa);
+        Assert.Equal(0, sDecimal.Exponent);
+        Assert.Equal(SDecimal.One, sDecimal);
+    }
+}
